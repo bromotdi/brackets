c7e6c59feaa4d4f6072408f85241356c425541fa
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2023-02-27 19:40:56 +0300

    feat: edit check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = check = (str, bracketsConfig) => {
+    const bracket = new Map(bracketsConfig); // Создаем коллекцию из пар скобок для удобного поиска
+    const array = [];
+    for (const i of str) {
+        (bracket.get(array.at(-1)) === i) ? array.pop(): array.push(i);
+        // Если пара последнего символа в стеке тот же что и в строке на данный момент
+        // удаляем его или добавляем в конец стека
+    }
+    return array.length === 0; // В конце проверяем пустой ли у нас стек
 }
